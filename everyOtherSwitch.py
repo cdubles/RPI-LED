ed043e306e07f3820815ca6658d8174ea8533356
Cody <33189090+cdubles@users.noreply.github.com>
2022-12-30 18:45:27 -0500

    Create everyOtherSwitch.py

---
+++ everyOtherSwitch.py
@@ -0,0 +1,12 @@
+import board
+import neopixel
+pixelCount = 150
+pixels = neopixel.NeoPixel(board.D18, pixelCount)
+mod =0;
+while True:
+    for x in range(0,pixelCount):
+        if((x+mod)%2==0):
+            pixels[x] = (255,0,0)
+        else:
+            pixels[x] = (0,0,255)
+        mod+=1
