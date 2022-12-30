25a84e0731cbb76d302a31723ad62cd810d7906a
Cody <33189090+cdubles@users.noreply.github.com>
2022-12-30 18:28:09 -0500

    move through random colors

--- randomColors.py
+++ randomColors.py
@@ -4,8 +4,9 @@ import random
 pixelCount = 150
 pixels = neopixel.NeoPixel(board.D18, pixelCount)
 
-for x in range(0,pixelCount):
-    R = random.randint(0,255)
-    G = random.randint(0,255)  
-    B = random.randint(0,255)
-    pixels[x] = (R,G,B)
+while True:
+    for x in range(0,pixelCount):
+        R = random.randint(0,255)
+        G = random.randint(0,255)  
+        B = random.randint(0,255)
+        pixels[x] = (R,G,B)
