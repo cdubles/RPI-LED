0305ddbb09e83f73105a92d677f1bcf927b07676
Cody <33189090+cdubles@users.noreply.github.com>
2022-12-30 14:42:09 -0500

    fix switch.py fill statements

--- switch.py
+++ switch.py
@@ -6,8 +6,8 @@ pixels = neopixel.NeoPixel(board.D18, pixelCount)
 
 while True:
     time.sleep(1)
-    pixels.fill(0,255,0)
+    pixels.fill((0,255,0))
     time.sleep(1)
-    pixels.fill(0,0,255)
+    pixels.fill((0,0,255))
 
 
