58ceceffcf58d92ce7ec1c41766e17bca2d7a2f1
Cody <33189090+cdubles@users.noreply.github.com>
2022-12-30 18:17:58 -0500

    add random pattern

---
+++ random.py
@@ -0,0 +1,11 @@
+import board
+import neopixel
+import random
+pixelCount = 150
+pixels = neopixel.NeoPixel(board.D18, pixelCount)
+
+for x in range(0,pixelCount):
+    R = random.randint(0,255)
+    G = random.randint(0,255)  
+    B = random.randint(0,255)
+    pixels[x] = (R,G,B)
