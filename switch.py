c1aacdda3d67437eeeafe0035d971f086790a67f
Cody <33189090+cdubles@users.noreply.github.com>
2022-12-30 14:40:44 -0500

    add switch pattern

---
+++ switch.py
@@ -0,0 +1,13 @@
+import board
+import neopixel
+import time
+pixelCount = 150
+pixels = neopixel.NeoPixel(board.D18, pixelCount)
+
+while True:
+    time.sleep(1)
+    pixels.fill(0,255,0)
+    time.sleep(1)
+    pixels.fill(0,0,255)
+
+
